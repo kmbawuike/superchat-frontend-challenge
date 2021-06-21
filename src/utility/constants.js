11c57fc6123b3fe8861574226d5f6413bc04ac81
kmbawuike <kmbawuike@gmail.com>
2021-06-21 07:25:46 +0100

    @add: added constants

---
+++ src/utility/constants.js
@@ -0,0 +1,14 @@
+export const icons = {
+  user_tie: "fas fa-user-tie",
+  user_tag: "fas fa-user-tag",
+  user_astronaut: "fas fa-user-astronaut",
+  user_plus: "fas fa-user-plus",
+  user_ninja: "fas fa-user-ninja",
+  user_circle: "fas fa-user-circle",
+  user_circle2: "far fa-user-circle",
+  user_slash: "fas fa-user-alt-slash",
+  user: "fas fa-user",
+  user2: "far fa-user",
+}
+
+export const base_url = "https://api.github.com/";
